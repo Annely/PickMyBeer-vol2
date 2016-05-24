6490583bec6681b5411f464dcb56c11c56ebbf98
Annely <annely.aasalaid@gmail.com>
2016-05-24 20:29:38 +0300

    test korras, tulemused array loodud aga ei näita ja järelikult ei salvesta?

---
+++ PROJEKT/views/tulemused.php
@@ -0,0 +1,10 @@
+<div>
+
+<php?
+	echo $_POST["aastaaeg"];
+	
+	$values = array();
+	array_push($values, $_POST["aastaaeg[]"], $_POST["plaanis]"],$_POST["eelistus[]"],$_POST["tuju[]"],$_POST["isu[]"],$_POST["staar[]"]]
+	echo $values;
+	?>
+</div>
